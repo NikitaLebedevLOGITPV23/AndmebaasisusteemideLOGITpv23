1614de2d098a0e9b68a16a1e85e35face2c33265
NikitaLebedevLOGITPV23 <154318763+NikitaLebedevLOGITPV23@users.noreply.github.com>
2025-04-15 12:43:13 +0300

    andmed lisatud.sql

--- SOLlaused.sql
+++ SOLlaused.sql
@@ -117,3 +117,135 @@ INSERT INTO inimene
 (nimi, synniaeg, telefon, pikkus, opilaskodu, autoID)
 VALUES
 ('Peter Su', '2020-03-14', '2568952', 90.5, 0, 1);
+
+-----------
+CREATE TABLE filmid(
+filmID int primary key identity(1,1),
+filmNimi varchar(30) unique,
+filmPikkus int,
+rezisoor varchar(30)
+);
+SELECT * FROM filmid;
+
+INSERT INTO filmid(filmNimi, filmPikkus, rezisoor)
+VALUES ('Minecraft', 120, 'Jared Hess');
+
+--protseduur mis lisab uus film ja kohe näitav tabelis (INSERT, SELECT)
+CREATE PROCEDURE lisaFilm
+@nimi varchar(30),
+@pikkus int,
+@rezisoor varchar(30)
+AS
+BEGIN
+INSERT INTO filmid(filmNimi, filmPikkus, rezisoor)
+VALUES (@nimi, @pikkus, @rezisoor);
+SELECT * FROM filmid;
+END;
+
+--kutse
+EXEC lisaFilm 'Minecraft 2', 300, 'noway';
+
+
+--remove procedure
+DROP Procedure lisaFilm;
+
+
+--proceduur, mis keustutab filmi filmiID järgi (DELETE, SELECT)
+CREATE PROCEDURE kustutaFilm
+@id int
+AS
+BEGIN
+SELECT * FROM filmid;
+DELETE FROM filmid WHERE filmID=@id;
+SELECT * FROM filmid;
+END;
+--kutse
+EXEC  kustutaFilm 1;
+EXEC kustutaFilm @id=1;
+
+--proceduur, mis uuendab filmiPikkus 5% suurendab
+CREATE PROCEDURE uuendaFilmiPikkus
+AS
+BEGIN
+SELECT * FROM filmid;
+UPDATE filmid SET filmPikkus=filmPikkus*1.05;
+SELECT * FROM filmid;
+END
+
+--kutse
+EXEC uuendaFilmiPikkus;
+
+--proceduur, mis uuendab filmiPikkus. Kasutaja sisestav väärtus
+CREATE PROCEDURE uuendaFilmiPikkus2
+@arv decimal(5,2)
+AS
+BEGIN
+SELECT * FROM filmid;
+UPDATE filmid SET filmPikkus=filmPikkus*@arv;
+SELECT * FROM filmid;
+END
+
+--kutse
+EXEC uuendaFilmiPikkus2 @arv=1.5;
+
+--protseduur, mis näitab filmid esimese tähe järgi
+CREATE PROCEDURE filmid1täht
+@taht char(1)
+AS
+BEGIN
+SELECT * FROM filmid
+WHERE filmNimi LIKE CONCAT(@taht, '%');
+END
+
+--kutse
+EXEC filmid1täht 'M';
+
+--protseduur, mis näitab filmid mis sisaldavad nimes sisestatud täht
+CREATE PROCEDURE filmidSiseldabTaht
+@taht char(1)
+AS
+BEGIN
+SELECT * FROM filmid
+WHERE filmNimi LIKE CONCAT('%', @taht, '%');
+END
+
+--kutse
+EXEC filmidSiseldabTaht 'i'
+
+--protseduur, mis näitab kesknmine filmide pikkus
+CREATE PROCEDURE keskminePikkus
+AS
+BEGIN
+SELECT AVG(filmPikkus) as 'Keskmine Pikkus' FROM filmid
+END
+
+--kutse
+EXEC keskminePikkus;
+
+--KERULINE PROTSEDUUR!!!
+--ALTER TABLE tabelinimi ADD veerg tyyp --veerg lisamine
+--ALTER TABLE tabelinimi DROP veerg --veergu kustutamine
+
+CREATE PROCEDURE tabeliMuudatus
+@tegevus varchar(10),
+@tabelinimi varchar(25),
+@veerunimi varchar(25),
+@tyyp varchar(25) =null
+AS
+BEGIN
+DECLARE @sqltegevus as varchar(max)
+set @sqltegevus=case 
+when @tegevus='add' then concat('ALTER TABLE ', @tabelinimi, ' ADD ', @veerunimi, ' ', @tyyp)
+when @tegevus='drop' then concat('ALTER TABLE ', @tabelinimi, ' DROP COLUMN ', @veerunimi)
+END;
+print @sqltegevus;
+begin 
+EXEC (@sqltegevus);
+END
+END;
+
+SELECT * FROM filmid;
+--добавление столбца
+EXEC tabeliMuudatus @tegevus='add', @tabelinimi='filmid', @veerunimi='test', @tyyp='int';
+--удаление столбца
+EXEC tabeliMuudatus @tegevus='drop', @tabelinimi='filmid', @veerunimi='test';
